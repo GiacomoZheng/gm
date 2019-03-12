545ca0134ea843616eca6f100d1c2fec7a89f9d7
GiacomoZheng <giacomo821@outlook.com>
2019-03-12 15:24:30 +0800

    before reinstall

--- .vscode/tree.py
+++ .vscode/tree.py
@@ -1,7 +1,14 @@
-# please cd to the gm to run this file
+# please cd to `gm` and run this file
 import os
 if not os.getcwd().endswith("\gm"): raise Exception("invalid location")
-with open("tree", "r", encoding = "utf8") as file:
+
+# import subprocess as sp
+# with open("tree", "wb") as file:
+# 	file.write(
+# 			sp.run(["tree"], shell=True, stdout=sp.PIPE).stdout
+# 			# .decode('windows-1252')
+# 		)
+with open("tree", "r", encoding = 'windows-1252') as file:
 	while not file.read(1) == "├":
 		file.readline()
 	# jump away
@@ -9,5 +16,3 @@ with open("tree", "r", encoding = "utf8") as file:
 
 with open("tree", "w", encoding = "utf8") as file:
 	file.write(text.replace("│", " ").replace("   ", "\t").replace(" ", ""))
-
-	
